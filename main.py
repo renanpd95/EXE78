ade84d3558ab24230b27f159743f9dbfa8b17cab
Renan <88853399+renanpd95@users.noreply.github.com>
2022-05-12 00:30:36 +0000

    Melhoria no código

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-
+import os
 n = 1
 soma = 0
 cont = 0
 while (n > 0):
     n = int(input("Digite um número:"))
-    
+    os.system('clear')
     if( n > 0):
       soma = soma + n
       cont = cont + 1
       media = soma / cont
 else:
-  print(media)
+  print("Média dos valores positivos",media)
   
